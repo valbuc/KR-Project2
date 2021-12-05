fe2f8cb4eca40e46c408196fb2209e493e32ce52
Taylor Doughty <t.m.doughty@student.vu.nl>
2021-12-05 21:55:49 +0100

    -

--- TD.py
+++ TD.py
@@ -3,6 +3,7 @@ from BNReasoner import BNReasoner
 import networkx as nx
 import copy
 import matplotlib.pyplot as plt
+from collections import Counter
 
 import os 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
@@ -26,66 +27,80 @@ dogproblem.get_all_edges()
 #edges --> connect the variables which appaer in the same factor
 
 
+def ordering_mindegree(self):
 
-bn = copy.deepcopy(dogproblem)
+    bn = copy.deepcopy(self.bn)
 
-var_neighbor = {}
-smallest = 0
-order=[]
-edges = bn.get_all_edges()
-print("the edges were:----", edges)
-for variable in bn.get_all_variables():
-    print("the currently selected variable is", variable)
-    count = 0
-    for tuple in edges:
-        if variable in tuple:
-            count +=1
-        var_neighbor[variable]=count
-    least = str(min(var_neighbor, key=var_neighbor.get)) #variable with least amount of neighbors
-    print("the current var with least amount of neighbors is:------", least)
-    ### if this variable appears in one tuple, that means you create no new edges
-    print(var_neighbor[variable])
-    new_edges_list=[]
-    if var_neighbor[variable] != 1:
-        for tuple in edges:
-            if variable in tuple:
-                new_edges_list.append(variable)
-                print(new_edges_list)
-                
+    var_neighbor = {}
+    order=[]
+    edges = bn.get_all_edges()
+    print("the edges were:----", edges)
 
+    edges = bn.get_all_edges()
+    bn.draw_structure()
 
+    interations = 1
+    for variable in bn.get_all_variables():
+        print("the currently selected variable is:----", variable)
 
-    ### if this variable appears in more than one tuple, you make an edge with the other values in these tuples
-
-
-    # temp_list=bn.get_all_variables()
-    # print(temp_list)
-    # temp_list.remove(least)
-    # print(temp_list)
-    # for var1 in temp_list:
-    #     print("this is var1:", var1)
-    #     for var2 in temp_list:
-    #     # var2=temp_list[-1]
-    #         print("this is var2:", var2)
-    #         if (var1, var2) in edges:
-    #             pass
-    #         try:
-    #             bn.add_edge((var1, var2))
-    #         except: 
-    #             pass
-
-edges= bn.get_all_edges()
-print("the edges are:----", edges)
-bn.draw_structure()
-graph = bn.get_structure().to_undirected()
-
-        # for var1 in temp_list[-1]:
-        #     print(var1)
-        #     bn.add_edge((var, var1))
+        # counts the amount of edges said variable has
+        count = 0
+        # for variable in bn.get_all_variables():
+        ok = Counter(elem[0] for elem in edges)
+        bruh = Counter(elem[1] for elem in edges)
+        okdict = dict(ok)
+        bruhdict = dict(bruh)
+        var_neighbor = {k: okdict.get(k, 0) + bruhdict.get(k, 0) for k in set(okdict) | set(bruhdict)}
+        # print("this is the dict:---", var_neighbor)
             
-# the edges were:---- [('bowel-problem', 'dog-out'), ('dog-out', 'hear-bark'), ('family-out', 'light-on'), ('family-out', 'dog-out')]
+        
+        # selects variable with least amount of edges/neighbors
+        least = str(min(var_neighbor, key=var_neighbor.get)) 
+        order.append(least)
+        print("the current var with least amount of neighbors is:------", least)
+        new_edges_list=[]
 
-# the edges are:---- [('bowel-problem', 'dog-out'), ('bowel-problem', 'hear-bark'), ('bowel-problem', 'family-out'), ('dog-out', 'hear-bark'), ('family-out', 'dog-out'), ('family-out', 'hear-bark')]
+        # if variable has non-adjacdent neighbors, add edges between them
+        if var_neighbor[least] != 1:
+            for tuple in edges:
+                if least in tuple:
+                    for i in tuple:
+                        if i != least:
+                            new_edges_list.append(i)
+                            print(new_edges_list)
+                            for var1 in new_edges_list:
+                                print("this is var1:", var1)
+                                for var2 in new_edges_list:
+                                    var2=new_edges_list[-1]
+                                    print("this is var2:", var2)
+                                    if (var1, var2) in edges:
+                                        pass
+                                        try:
+                                            bn.add_edge((var1, var2))
+                                        except: 
+                                            pass
+
+        del var_neighbor[least] #deletes from dict
+        print("updated dict:----", var_neighbor)
+        bn.del_var(least) # deletes from bn
+        edges= bn.get_all_edges()
+        bn.draw_structure()
+        graph = bn.get_structure().to_undirected()
+        print("the order is:", order)
+        print("the edges are:----", edges)
+        
+
+
+
+
+    edges= bn.get_all_edges()
+    print("the edges are:----", edges)
+    bn.draw_structure()
+    graph = bn.get_structure().to_undirected()
+    print(least)
+    if len(edges) == 0:
+        return
+
 
 
 
