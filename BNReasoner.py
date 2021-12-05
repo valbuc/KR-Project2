085e4469c0fe7264c050e6abcb2eb12e4aa27a50
Taylor Doughty <t.m.doughty@student.vu.nl>
2021-12-05 23:44:13 +0100

    working on min-fill heuristic

--- BNReasoner.py
+++ BNReasoner.py
@@ -71,6 +71,7 @@ class BNReasoner:
 
         """
         returns ordering of elimination list
+        the min-degree heuristic creates the order based on first eliminating the nodes with the least amount of neighbors
         """
 
         bn = copy.deepcopy(self.bn)
@@ -122,6 +123,78 @@ class BNReasoner:
                                             except: 
                                                 pass
 
+
+            bn.draw_structure()                              
+            del var_neighbor[least] # deletes variable from dict
+            bn.del_var(least) # deletes variable from bn
+            count += 1
+            print("updated dict:---", var_neighbor)
+
+
+    def ordering_minfull(self) -> List[str]:
+
+        """
+        WORK IN PROGRESS
+        returns ordering of elimination list
+        the min-full heuristic creates the order based on first eliminating the variables that add the smallest number of edges
+        """
+
+        bn = copy.deepcopy(self.bn)
+        self.order=[]
+
+        count = 1
+        for variable in bn.get_all_variables():
+            edges = bn.get_all_edges()
+            print("this is iteration:---", count)
+            print("the current variable:---", variable)
+
+            # when at final variable, returns list
+            if len(edges) == 0:
+                self.order.append(variable)
+                print("this is the ordering:---", self.order)
+                return(self.order)
+            else:
+                pass
+
+            # makes dict which holds variables and amount of edges they have                
+            first = Counter(elem[0] for elem in edges)
+            second = Counter(elem[1] for elem in edges)
+            firstdict = dict(first)
+            seconddict = dict(second)
+            var_neighbor = {k: firstdict.get(k, 0) + seconddict.get(k, 0) for k in set(firstdict) | set(seconddict)}
+            print("variables and amount edges at start of iteration:---", var_neighbor)
+                
+            # selects variable with least amount of edges/neighbor
+            least = str(min(var_neighbor, key=var_neighbor.get)) 
+            print("the variable with the least edges is:---", least)
+            self.order.append(least)
+            new_edges_list=[]
+
+            # if variable has non-adjacdent neighbors (more than one edge connection), add edges between them
+            # if only has one connection, continue
+
+            # stairway to literal hell 
+            edges_to_add=0
+            connect_dict={}
+            if var_neighbor[least] != 1:
+                for tuple in edges: 
+                    if least in tuple:
+                        for i in tuple:
+                            if i != least:
+                                new_edges_list.append(i)                            
+                                for var1 in new_edges_list:                               
+                                    for var2 in new_edges_list:
+                                        var2=new_edges_list[-1]                                      
+                                        if (var1, var2) in edges:
+                                            pass
+                                            try:
+                                                bn.add_edge((var1, var2))
+                                                edges_to_add += 1
+                                                connect_dict.append[var1]=edges_to_add
+                                            except: 
+                                                pass
+
+                least = str(min(edges_to_add, key=edges_to_add.get)) 
 
             bn.draw_structure()                              
             del var_neighbor[least] # deletes variable from dict
