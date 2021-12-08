669191df09fa263bdf5389961852431a4cef53d8
valbuc <90454420+valbuc@users.noreply.github.com>
2021-12-08 13:22:59 +0100

    dseparation improved

--- BNReasoner.py
+++ BNReasoner.py
@@ -27,35 +27,44 @@ class BNReasoner:
         of y given z
         """
         xyz = x + y + z
-        bn = copy.deepcopy(self.bn)
-        #bn.draw_structure()
-        continueiter = True
-        # first save all items to be delted in list and then iterate over that list to delete 
-        while continueiter == True:
+        cpbn = copy.deepcopy(self.bn)
+        # bn.draw_structure()
+        # first save all items to be delted in list and then iterate over that list to delete
+        while True:
+            delete = []
             continueiter = False
-            for variable in self.bn.get_all_variables():
-                children = [1]
-                try:
-                    children = bn.get_children(variable)
-                except:
-                    pass
+            for variable in cpbn.get_all_variables():
+                # children = [1]
+                # try:
+                children = cpbn.get_children(variable)
+                # except:
+                #    pass
                 if len(children) == 0:
                     if variable not in xyz:
-                        try:
-                            bn.del_var(variable)
-                        except:
-                            pass
-                        for edge in bn.get_all_edges():
-                            if variable in edge:
-                                bn.del_edge(edge)
-                        continueiter = True
-            for variable in z:
-                for edge in bn.get_all_edges():
-                    if edge[0] == variable:
-                        bn.del_edge(edge)
+                        delete.append(variable)
+                        # try:
+                        #   bn.del_var(variable)
+                        # except:
+                        #    pass
+            if len(delete) == 0:
+                break
 
-        #bn.draw_structure()
-        graph = bn.get_structure().to_undirected()
+            for variable in delete:
+                cpbn.del_var(variable)
+                delete_edge = []
+                for edge in cpbn.get_all_edges():
+                    if variable in edge:
+                        delete_edge.append(edge)
+                for edge in delete_edge:
+                    cpbn.del_edge(edge)
+
+        for variable in z:
+            for edge in cpbn.get_all_edges():
+                if edge[0] == variable:
+                    cpbn.del_edge(edge)
+
+        # bn.draw_structure()
+        graph = cpbn.get_structure().to_undirected()
         # graph = bn.get_interaction_graph()
 
         for X in x:
@@ -66,8 +75,6 @@ class BNReasoner:
 
         return True
 
-
-
     def ordering_mindegree(self) -> List[str]:
 
         """
@@ -76,11 +83,11 @@ class BNReasoner:
         """
 
         bn = copy.deepcopy(self.bn)
-        self.order=[]
+        self.order = []
 
         count = 1
         for variable in bn.get_all_variables():
-            bn.draw_structure() 
+            bn.draw_structure()
             edges = bn.get_all_edges()
             print("this is iteration:---", count)
             print("the current variable:---", variable)
@@ -89,47 +96,49 @@ class BNReasoner:
             if len(edges) == 0:
                 self.order.append(variable)
                 print("this is the ordering:---", self.order)
-                return(self.order)
+                return self.order
             else:
                 pass
 
-            # makes dict which holds variables and amount of edges they have                
+            # makes dict which holds variables and amount of edges they have
             first = Counter(elem[0] for elem in edges)
             second = Counter(elem[1] for elem in edges)
             firstdict = dict(first)
             seconddict = dict(second)
-            var_neighbor = {k: firstdict.get(k, 0) + seconddict.get(k, 0) for k in set(firstdict) | set(seconddict)}
+            var_neighbor = {
+                k: firstdict.get(k, 0) + seconddict.get(k, 0)
+                for k in set(firstdict) | set(seconddict)
+            }
             print("variables and amount edges at start of iteration:---", var_neighbor)
-                
+
             # selects variable with least amount of edges/neighbor
-            least = str(min(var_neighbor, key=var_neighbor.get)) 
+            least = str(min(var_neighbor, key=var_neighbor.get))
             print("the variable with the least edges is:---", least)
             self.order.append(least)
-            new_edges_list=[]
+            new_edges_list = []
 
             # if variable has non-adjacdent neighbors (more than one edge connection), add edges between them
             # if only has one connection, continue
-            # going to work on making this more efficient   
+            # going to work on making this more efficient
             if var_neighbor[least] != 1:
                 for tuple in edges:
                     if least in tuple:
                         for i in tuple:
                             if i != least:
-                                new_edges_list.append(i)                            
-                                for var1 in new_edges_list:                               
+                                new_edges_list.append(i)
+                                for var1 in new_edges_list:
                                     for var2 in new_edges_list:
-                                        var2=new_edges_list[-1]                                      
+                                        var2 = new_edges_list[-1]
                                         if (var1, var2) in edges:
                                             pass
                                             try:
                                                 bn.add_edge((var1, var2))
-                                            except: 
-                                                pass                            
-            del var_neighbor[least] # deletes variable from dict
-            bn.del_var(least) # deletes variable from bn
+                                            except:
+                                                pass
+            del var_neighbor[least]  # deletes variable from dict
+            bn.del_var(least)  # deletes variable from bn
             count += 1
             print("updated dict:---", var_neighbor)
-
 
     def ordering_minfull(self) -> List[str]:
 
@@ -140,11 +149,11 @@ class BNReasoner:
         """
 
         bn = copy.deepcopy(self.bn)
-        self.order=[]
+        self.order = []
 
         count = 1
         for variable in bn.get_all_variables():
-            bn.draw_structure() 
+            bn.draw_structure()
             edges = bn.get_all_edges()
             print("this is iteration:---", count)
             print("the current variable:---", variable)
@@ -153,53 +162,56 @@ class BNReasoner:
             if len(edges) == 0:
                 self.order.append(variable)
                 print("this is the ordering:---", self.order)
-                return(self.order)
+                return self.order
             else:
                 pass
 
-            # makes dict which holds variables and amount of edges they have             
+            # makes dict which holds variables and amount of edges they have
             first = Counter(elem[0] for elem in edges)
             second = Counter(elem[1] for elem in edges)
             firstdict = dict(first)
             seconddict = dict(second)
-            var_neighbor = {k: firstdict.get(k, 0) + seconddict.get(k, 0) for k in set(firstdict) | set(seconddict)}
+            var_neighbor = {
+                k: firstdict.get(k, 0) + seconddict.get(k, 0)
+                for k in set(firstdict) | set(seconddict)
+            }
             print("variables and amount edges at start of iteration:---", var_neighbor)
-                
+
             # selects variable with least amount of edges/neighbor
-            least = str(min(var_neighbor, key=var_neighbor.get)) 
+            least = str(min(var_neighbor, key=var_neighbor.get))
             print("the variable with the least edges is:---", least)
             self.order.append(least)
-            new_edges_list=[]
+            new_edges_list = []
 
             # if variable has non-adjacdent neighbors (more than one edge connection), add edges between them
             # if only has one connection, continue
 
-            # stairway to literal hell 
-            edges_to_add=0
-            connect_dict={}
+            # stairway to literal hell
+            edges_to_add = 0
+            connect_dict = {}
             if var_neighbor[least] != 1:
-                for tuple in edges: 
+                for tuple in edges:
                     if least in tuple:
                         for i in tuple:
                             if i != least:
-                                new_edges_list.append(i)                            
-                                for var1 in new_edges_list:                               
+                                new_edges_list.append(i)
+                                for var1 in new_edges_list:
                                     for var2 in new_edges_list:
-                                        var2=new_edges_list[-1]                                      
+                                        var2 = new_edges_list[-1]
                                         if (var1, var2) in edges:
                                             pass
                                             try:
                                                 bn.add_edge((var1, var2))
                                                 edges_to_add += 1
-                                                connect_dict.append[var1]=edges_to_add
-                                            except: 
+                                                connect_dict.append[var1] = edges_to_add
+                                            except:
                                                 pass
 
                 least = str(min(edges_to_add, key=edges_to_add.get))
                 del edges_to_add[least]
             else:
                 del var_neighbor[least]
-                             
-            bn.del_var(least) # deletes variable from bn
+
+            bn.del_var(least)  # deletes variable from bn
             count += 1
             print("updated dict:---", var_neighbor)
